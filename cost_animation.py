9d0de0ed57eac698ba34816840bc73393aea996b
Eitan <vilker@Eitans-MacBook-Air.local>
2018-05-21 16:49:43 -0400

    Completed cost_animation structure. Now need to make sure math is accurate, find robot image, and figure out how to move robot straight

--- cost_animation.py
+++ cost_animation.py
@@ -2,15 +2,108 @@ from cs1lib import *
 from time_cost import *
 from robot import *
 
-
 TIMESTEP = 1
 
 robot = Robot(200, 200, 5, 10, 10, 5, 45)
+step = 0;
+
+
+def calculate_arctan(xs, ys, xg, yg):
+    arctan = (atan2(ys-yg, xs-xg))*360/(2*pi)
+    return arctan
+
+
+def calculate_initial_rotation(theta_s, theta_f):
+    if (theta_s - theta_f) <= 90:
+        print("1) clockwise 2) forward")
+        return -(theta_s - theta_f)
+
+    if 180 > (theta_s - theta_f) > 90:
+        print("1) counterclockwise 2) backwards")
+        return theta_s - theta_f
+
+    if 270 > (theta_s - theta_f) > 180:
+        print("1) clockwise 2) backwards")
+        return -(theta_s - theta_f)
+
+    if 360 > (theta_s - theta_f) >= 270:
+        print("1) counterclockwise 2) forward")
+        return theta_s - theta_f
+
+    # Boundary cases
+    if (theta_s - theta_f) == (0 or 360):
+        print("1) none 2) forward")
+        return 0
+
+    if (theta_s - theta_f) == 180:
+        print("1) none 2) backwards")
+        return 0
+
+
+def calculate_second_rotation(theta_g, theta_f):
+    if 180 >= (theta_g - theta_f) > 0:
+        print("3) clockwise. Final: forwards")
+        return theta_f - theta_g
+    if 360 > (theta_g - theta_f) > 180:
+        return theta_g - theta_f
+        print("3) counterclockwise. Final: forwards")
+    if (theta_g - theta_f) == (0 or 360):
+        print("3) none. Final: fowards")
+        return 0
 
 
 def main():
+    global step
+    theta_s = 30
+    theta_g = 45
+    xs = 10
+    ys = 30
+    xg = 25
+    yg = 20
+    theta_f = calculate_arctan(xs, ys, xg, yg)
+
     robot.draw_robot()
     robot.step(TIMESTEP)
+    if step == 0:
+        angle = calculate_initial_rotation(theta_s, theta_f)
+        if angle > 0:
+            robot.theta += TIMESTEP * robot.w
+            if robot.theta >= angle:
+                step = 0.5
+        elif angle < 0:
+            robot.theta -= TIMESTEP * robot.w
+            if robot.theta <= angle:
+                step = 0.5
+        else:
+            step = 0.5
+#
+#
+#
+#
+#
+#
+    if step == 0.5:
+        # Move straight- not sure how exactly to do this
+        step = 1
+#
+#
+#
+#
+#
+
+    if step == 1:
+        angle = calculate_second_rotation(theta_g, theta_f)
+        if angle > 0:
+            robot.theta += TIMESTEP * robot.w
+            if robot.theta >= angle:
+                step = 2
+        elif angle < 0:
+            robot.theta -= TIMESTEP * robot.w
+            if robot.theta <= angle:
+                step = 2
+        else:
+            step = 2
+
 
 start_graphics(main)
 
--- robot.py
+++ robot.py
@@ -3,6 +3,8 @@ from cs1lib import *
 
 class Robot:
 
+    image = load_image(filename)
+
     def __init__(self, x, y, r, vx, vy, w, theta):
         self.x = x;
         self.y = y;
@@ -19,4 +21,4 @@ class Robot:
 
     def draw_robot(self):
         set_fill_color(0, 0, 1)
-        draw_circle(self.x, self.y, self.r)
+        draw_image(image, self.x, self.y, self.r, cx, cy, self.theta)
